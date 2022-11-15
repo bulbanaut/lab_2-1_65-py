5b33017bc2f2d9ad7f38eb5b63f2d54bca42fb3b
bulbanaut <aleks13102004@gmail.com>
2022-11-16 01:45:32 +0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#! /bin/python
+#! /usr/bin/python
 import math
 
 a = int(input('Значение k:'))
@@ -9,11 +9,8 @@ r = int(input('Значение r:'))
 x0 = -(a*c / (a*a + b*b))
 y0 = -(b*c / (a*a + b*b))
 
-if (c*c > r*r*(a*a+b*b) + 0.00000001):
-    print('нет точек')
-elif (abs(c*c - r*r*(a*a - b*b)) < 0.00000001):
-    print('одна точка')
-    print(x0, y0)
+if c*c > r*r*(a*a+b*b):
+    print('точек нет')
 else:
     d = r*r - c*c/(a*a + b*b)
     mult = math.sqrt(d / (a*a + b*b))
@@ -21,4 +18,7 @@ else:
     bx = x0 - b*mult
     ay = y0 - a*mult
     by = y0 + a*mult
-    print('две точки\n', ax, ay, '\n', bx, by)
+    if ax - bx == 0 and ay - by ==0:
+        print('одна точка\n', bx, by)
+    elif ax - bx +1 != 0 and ay - by !=0:
+        print('две точки\n', ax, ay, '\n', bx, by)
